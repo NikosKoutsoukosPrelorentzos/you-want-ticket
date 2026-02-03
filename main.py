11d1ec39c6715124f1064d96df126809e769971b
Nikos Koutsoukos <nikos.koutsoukos@mojagroup.com>
2026-02-03 21:31:36 +0200

    Configure CORS middleware for development
    - Import `CORSMiddleware` from `starlette.middleware.cors` in `main.py`.
    - Add middleware to the FastAPI application to allow all origins, credentials, methods, and headers.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_router
 from app.core.config import settings
@@ -26,3 +27,12 @@ app = FastAPI(
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (only for development!)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
